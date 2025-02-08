bca760ad1f8e4db3384ad418212498fda6aeebbd
Simfort <lemondzavadavid265@gmail.com>
2025-02-08 19:09:35 +0300

    added creator invoices

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 import { useEffect } from "react";
 import FormLogin from "../ui/login/FormLogin";
-import "@/images/airplnae.png";
 import { redirect } from "next/navigation";
 export default function Page() {
   useEffect(() => {
@@ -10,7 +9,7 @@ export default function Page() {
     }
   }, []);
   return (
-    <main className=" bg-scroll h-screen bg-[url('@/images/airplnae.png')] bg-cover bg-center flex justify-center  w-1/1">
+    <main className=" h-screen  flex justify-center  w-1/1">
       <section className="flex   justify-center items-center flex-col">
         <h1 className="text-[100px]">Login</h1>
         <FormLogin />
